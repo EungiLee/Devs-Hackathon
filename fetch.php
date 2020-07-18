0440c5b0745bad689bd728b0e8595707e68cf637
logmell <loganmellow@gmail.com>
2020-07-18 17:08:00 +1200

    added test html file

--- fetch.php
+++ fetch.php
@@ -1,10 +1,10 @@
 <?php
-$link = mysqli_connect('warriors88.powwebmysql.com', 'llne', 'benmatech', 'devs_hackathon'); 
+    $link = mysqli_connect('warriors88.powwebmysql.com', 'llne', 'benmatech', 'devs_hackathon'); 
 	if (!$link) { 
     	die('Could not connect: ' . mysql_connect_error()); 
 	} 
 	
-	$sql = "SELECT * FROM `topics`";
+	$sql = "SELECT * FROM `topics` ORDER BY `ordering` ASC";
 	$result = mysqli_query($link, $sql);
 
     while($row = mysqli_fetch_assoc($result)) {
